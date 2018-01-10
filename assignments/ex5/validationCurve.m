bb476eed1442e7566ed76006355b4df8c83b2b03
alexjolong <alexjolong@gmail.com>
2018-01-09 20:49:14 -0600

    That was probably the easiest assignment yet

--- assignments/ex5/validationCurve.m
+++ assignments/ex5/validationCurve.m
@@ -39,14 +39,14 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
-
-
-
-
-
-
-
-
+for i=1:length(lambda_vec)
+    lambda = lambda_vec(i);
+    theta = trainLinearReg(X, y, lambda);
+    [J, grad] = linearRegCostFunction(X, y, theta, 0);
+    [Jcross, gradCross] = linearRegCostFunction(Xval, yval, theta, 0);
+    error_train(i) = J;
+    error_val(i) = Jcross;
+end
 
 % =========================================================================
 
